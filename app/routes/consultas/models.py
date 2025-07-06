c2c7f4b36015454ba05000dae48cd13c68e2b9f4
Eduardo Follmer <cheilarosa86@hotmail.com>
2025-07-05 23:40:03 -0300

    Desenvolvidade cadastro e gerenciamento de consulta

--- app/routes/consultas/models.py
+++ app/routes/consultas/models.py
@@ -13,6 +13,7 @@ def criar_tabelas_consulta():
                 id_profissional INTEGER NOT NULL,
                 data TEXT NOT NULL,
                 especialidade TEXT NOT NULL,
+                status INTEGER DEFAULT (0),
                 FOREIGN KEY(id_paciente) REFERENCES pacientes(id),
                 FOREIGN KEY(id_profissional) REFERENCES profissionais_saude(id)
             )
@@ -20,11 +21,23 @@ def criar_tabelas_consulta():
         conn.commit()
 
 # Funções para histórico clínico:
-def adicionar_consulta(id_paciente, data, especialidade):
+def adicionar_consulta(data):
+
     with sqlite3.connect(DB_NAME) as conn:
         cursor = conn.cursor()
         cursor.execute('''
-            INSERT INTO consultas (id_paciente, data, especialidade)
-            VALUES (?, ?, ?)
-        ''', (id_paciente, data, especialidade))
+            INSERT INTO consultas (id_paciente, id_profissional, data, especialidade)
+            VALUES (?, ?, ?, ?)
+        ''', (data["id_paciente"], data["id_profissional"], data["data"], data["especialidade"]))
         conn.commit()
+
+def alterar_status_consulta(id_consulta, status):
+    with sqlite3.connect(DB_NAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            UPDATE consultas
+            SET status = ?
+            WHERE id = ?
+        ''', (status, id_consulta))
+        conn.commit()
+        return cursor.rowcount
